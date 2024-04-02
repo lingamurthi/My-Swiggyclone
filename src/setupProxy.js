179012dc27b1ede0c3f6cb72c5db6c25a2b10cc2
lingamurthi <lingamurthy5364@gmail.com>
2024-04-02 08:46:55 +0530

    "new changes "

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/restaurants', // Match any request starting with '/restaurants'
     createProxyMiddleware({
-      target: 'https://my-swiggybackend.onrender.com', // Your backend server URL
+      target: 'my-swiggybackend.onrender.com', // Your backend server URL
       changeOrigin: true, // Needed for virtual hosted sites
       pathRewrite: {
         '^/restaurants': '/api/restaurantsmenu' // Rewrite '/restaurants' to '/api/restaurantsmenu'
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX0
 //export const MENU_API='https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=14.444057&lng=75.908034&restaurantId='
 
 
-export const MENU_API='/api/restaurantsmenu/'
+export const MENU_API='http://localhost:8000/api/restaurantsmenu/'
